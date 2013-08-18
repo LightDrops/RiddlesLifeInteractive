a23881cec1f193e3c0df3ab9ee1089fc37822cac
ReDrops <c.light93@ymail.com>
2013-08-18 20:41:53 +0530

    updated <3

--- push.bat
+++ push.bat
@@ -1,12 +1,12 @@
 @echo off
 xcopy /s /y E:\xampp\htdocs\RiddlesInteractive\style style\
 xcopy /y E:\xampp\htdocs\RiddlesInteractive\images images\
-xcopy /y E:\xampp\htdocs\RiddlesInteractive\js js\
+xcopy /y E:\xampp\htdocs\RiddlesInteractive\scripts js\
 copy /y E:\xampp\htdocs\RiddlesInteractive\index.html index.html
 git remote set-url origin git@github.com:ReDrops/RiddlesLifeInteractive.git
 git add index.html
 git add style\* 
-git add js\*
+git add scripts\*
 git add images\*
 git commit -a -m "updated <3"
 git push origin gh-pages
